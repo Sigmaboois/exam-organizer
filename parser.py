29c6e4a24004e2567bfe0dad7c0df39ff99e4307
arenzya <elshafeigasser@gmail.com>
2026-09-24 21:36:27 +0300

    Fixed logic of parser.py to extract 2nd page to find the code of the exam

--- parser.py
+++ parser.py
@@ -3,9 +3,15 @@ from pypdf import PdfReader as reader
 def meta_extract(pdf_path):
 
     pdf = reader(pdf_path)
+    
     page_1 = pdf.pages[0]
     page_1_text = page_1.extract_text()
-    lines = page_1_text.split("\n")
+    lines_1 = page_1_text.split("\n")
+    
+    page_2 = pdf.pages[1]
+    page_2_text = page_2.extract_text()
+    lines_2 = page_2_text.split("\n")
+    
     metadata = {
         "year":None,
         "subject_name":None,
@@ -14,9 +20,12 @@ def meta_extract(pdf_path):
         "paper_type":None
     }
 
-    for line in lines:
+    for line in lines_1:
         print(line)
-
+    
+    for line in lines_2:
+        print(line)
+    
     return metadata
 
 if __name__ == "__main__":
