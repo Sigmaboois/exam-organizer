7592fe323aecded1131f79b29744965785cda652
arenzya <elshafeigasser@gmail.com>
2026-09-24 18:07:45 +0300

    Added reader.py with basic functions to extract text from demo pdf

--- reader.py
+++ reader.py
@@ -2,8 +2,9 @@ from pypdf import PdfReader as reader
 
 input_path = input("Please input the path of ur file (paste in full directory if not in the same folder as reader.py)")
 
-pages = reader.pages()
+pdfreader = reader(input_path)
+pagesinpdf = pdfreader.pages
 
-for i in pages:
-    text = pages.extract_text
+for page in pagesinpdf:
+    text = page.extract_text()
     print(text)
