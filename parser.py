091c97958bb7bb7bd5c78efc790361f8d83a8c5f
arenzya <elshafeigasser@gmail.com>
2026-09-24 22:33:32 +0300

    Added more rules for extraction of information from first 2 pages

--- parser.py
+++ parser.py
@@ -11,25 +11,40 @@ def meta_extract(pdf_path):
     page_2 = pdf.pages[1]
     page_2_text = page_2.extract_text()
     lines_2 = page_2_text.split("\n")
+
+    code = lines_2[1]
+    code = code.split("/")
     
     metadata = {
         "year":None,
         "subject_name":None,
         "subject_code":None,
         "session":None,
-        "paper_type":None
+        "paper_type":None,
+        "paper":None,
+        "variant":None
     }
 
+    subject_name,subject_code = lines_1[3].split()
+    subject_code,paper = subject_code.split("/")
+    paper,variant = paper[0],paper[1]
+    
+    #For the paper_type key -->
     for line in lines_1:
         if "You must answer on the question paper" in line:
-            metadata["paper_type"] = "qp"
+            metadata["paper_type"] = "QP"
+
     
-    #for line in lines_2:
-     #   print(line)
     
-    return metadata
+    metadata["subject_name"] = subject_name
+    metadata["subject_code"] = subject_code
+    metadata["paper"] = paper
+    metadata["variant"] = variant
+
+    return metadata,code
 
 if __name__ == "__main__":
     print("This is a test\n")
-    temp = meta_extract("C:\\Users\\gasse\\OneDrive\\Documents\\Practice\\Learning git\\Exam Organizer\\9709_s25_qp_53.pdf")
-    print(temp)
+    metadata,code = meta_extract("C:\\Users\\gasse\\OneDrive\\Documents\\Practice\\Learning git\\Exam Organizer\\9709_s25_qp_53.pdf")
+    print(metadata,"\n",code)
+    
