17fb32e016f1128bd5d3d95bdec4c068ec3ff7fb
arenzya <elshafeigasser@gmail.com>
2026-09-24 23:30:57 +0300

    Fixed Fundamental issues in logic of parsing for MS papers and refined QP parsing

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ from pypdf import PdfReader as reader
 
 def meta_extract(pdf_path):
 
+    # INITIALIZATION OF ALL VARIABLES BEFORE BEING ASSIGNED
     pdf = reader(pdf_path)
     
     page_1 = pdf.pages[0]
@@ -11,9 +12,6 @@ def meta_extract(pdf_path):
     page_2 = pdf.pages[1]
     page_2_text = page_2.extract_text()
     lines_2 = page_2_text.split("\n")
-
-    code = lines_2[1]
-    code = code.split("/")
     
     metadata = {
         "year":None,
@@ -25,26 +23,38 @@ def meta_extract(pdf_path):
         "variant":None
     }
 
-    subject_name,subject_code = lines_1[3].split()
-    subject_code,paper = subject_code.split("/")
-    paper,variant = paper[0],paper[1]
-    
+    #END OF INITIALIZATION
+    #______________________________________________________________________________________________________________
+
     #For the paper_type key -->
     for line in lines_1:
         if "You must answer on the question paper" in line:
             metadata["paper_type"] = "QP"
-
+        elif "MARK SCHEME" in lines_1:
+            metadata["paper_type"] = "MS"
     
-    
-    metadata["subject_name"] = subject_name
-    metadata["subject_code"] = subject_code
-    metadata["paper"] = paper
-    metadata["variant"] = variant
+    # Extraction based on the fact that paper_type = QP
+    if metadata["paper_type"] ==  "QP": 
+        code = lines_2[1]
+        code = code.split("/")
+        year_expression = code[5]
+        yearsplitted = year_expression.split()
+        year = yearsplitted[2]
+        subject_name,subject_code = lines_1[3].split()
+        subject_code,paper = subject_code.split("/")
+        paper,variant = paper[0],paper[1]
 
-    return metadata,code
+        metadata["session"] = code[2]
+        metadata["subject_name"] = subject_name
+        metadata["subject_code"] = subject_code
+        metadata["paper"] = paper
+        metadata["variant"] = variant
+        metadata["year"] = year
+
+    return metadata
 
 if __name__ == "__main__":
     print("This is a test\n")
-    metadata,code = meta_extract("C:\\Users\\gasse\\OneDrive\\Documents\\Practice\\Learning git\\Exam Organizer\\9709_s25_qp_53.pdf")
-    print(metadata,"\n",code)
+    metadata= meta_extract("9709_w20_ms_12.pdf")
+    print(metadata)
     
